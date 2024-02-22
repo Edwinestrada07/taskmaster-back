4a638bd39ab33d5b7e7bbc5617f3d52eb9f364a7
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-02-22 11:17:24 -0500

    Anexo de los endpoint para subir foto, y para cambiar el password

--- user/user.js
+++ user/user.js
@@ -68,4 +68,57 @@ app.delete('/user/:id', validateToken, async (req, res) => {
     res.send({ status: 'success' })
 })
 
+// Ruta para cambiar la contraseña del usuario
+app.put('/change-password', validateToken, async (req, res) => {
+    // Obtener el ID de usuario del token JWT
+    const userId = req.user.id;
+  
+    // Obtener la contraseña actual, la nueva contraseña y el ID del usuario del cuerpo de la solicitud
+    const { currentPassword, newPassword } = req.body;
+  
+    try {
+        // Verificar si la contraseña actual es válida
+        const user = await pool.query('SELECT * FROM users WHERE id = $1', [userId]);
+        const hashedPassword = user.rows[0].password;
+        const passwordMatch = await bcrypt.compare(currentPassword, hashedPassword);
+    
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Contraseña actual incorrecta' });
+        }
+    
+        // Hashear la nueva contraseña
+        const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    
+        // Actualizar la contraseña en la base de datos
+        await pool.query('UPDATE users SET password = $1 WHERE id = $2', [hashedNewPassword, userId]);
+    
+        res.status(200).json({ message: 'Contraseña actualizada correctamente' });
+
+    } catch (error) {
+        console.error('Error al cambiar la contraseña:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
+
+// Ruta para cargar una imagen de perfil
+app.post('/upload-profile-image', validateToken, async (req, res) => {
+    // Obtener el ID de usuario del token JWT
+    const userId = req.user.id;
+  
+    // Obtener el archivo de imagen del cuerpo de la solicitud
+    const profileImage = req.file;
+  
+    try {
+      // Guardar la URL de la imagen de perfil en la base de datos o en algún sistema de almacenamiento
+      // En este ejemplo, supongamos que guardamos la URL en la base de datos
+      await pool.query('UPDATE users SET profile_image_url = $1 WHERE id = $2', [profileImage.path, userId]);
+  
+      res.status(200).json({ message: 'Imagen de perfil actualizada correctamente' });
+    } catch (error) {
+      console.error('Error al subir la imagen de perfil:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+  
 export default app
