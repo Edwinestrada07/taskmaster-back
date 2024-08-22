42eb3f22a2b07cba94a698af0b9ba3bf11ce0b2f
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-08-22 11:49:10 -0500

    update index.js

--- connect.js
+++ connect.js
@@ -1,44 +1,26 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import Sequelize from 'sequelize'
 
-// Cargar las variables de entorno
-dotenv.config();
-
-// Obtén la URL de la base de datos desde las variables de entorno
-const databaseUrl = process.env.DATABASE_URL;
-
-// Verificar la URL de la base de datos
-console.log('DATABASE_URL:', databaseUrl);
-
-// Verificar si la URL está definida
-if (!databaseUrl) {
-  throw new Error('DATABASE_URL is not defined in .env file');
-}
-
-// Crear una instancia de Sequelize
+// Usa la URL de conexión proporcionada por Supabase desde las variables de entorno
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
-  },
-  protocol: 'postgres'
-});
+})
+
+// Conexión a la base de datos por medio de Sequelize
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Conectado a la base de datos con éxito.')
+    })
+    .catch((error) => {
+        console.error('Error al conectar a la base de datos:', error)
+    })
+
+export default sequelize
 
 
-// Verificar la conexión
-const testConnection = async () => {
-  try {
-    console.log('Attempting to connect to the database...');
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-};
-
-testConnection();
-
-export default sequelize;
--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 import express  from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import AuthRouter from './auth/auth.js'
 import UserRouter from './user/user.js'
 import TaskRouter from './task/task.js'
 
+dotenv.config()
+
 const app = express()
 
 app.use(cors({ origin: '*' }))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(AuthRouter)
 app.use(UserRouter)
 app.use(TaskRouter)
 
-//Puerto del servidor
-app.listen(5000, () => {
-    console.log(`Aplicación conectada en el puerto ${5000}`)
-  })
+// Servir archivos estáticos desde la carpeta 'dist'
+app.use(express.static('dist'))
+
+// Ruta para verificar el funcionamiento del servidor
+app.get('/', (req, res) => {
+  res.send('El servidor está funcionando')
+})
+
+// Puerto del servidor
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Aplicación conectada en el puerto ${PORT}`)
+})
