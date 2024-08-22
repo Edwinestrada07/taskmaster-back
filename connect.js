59c403e364df08f72eb12397815e3f2dbde4b657
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-08-22 11:17:20 -0500

    update connect BD

--- connect.js
+++ connect.js
@@ -1,27 +1,44 @@
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-// Usa la URL de conexión proporcionada por Supabase desde las variables de entorno
+// Cargar las variables de entorno
+dotenv.config();
+
+// Obtén la URL de la base de datos desde las variables de entorno
+const databaseUrl = process.env.DATABASE_URL;
+
+// Verificar la URL de la base de datos
+console.log('DATABASE_URL:', databaseUrl);
+
+// Verificar si la URL está definida
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL is not defined in .env file');
+}
+
+// Crear una instancia de Sequelize
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // Deshabilitar la verificación SSL
-        }
-    },
-    logging: false // Opcional: desactiva el log de Sequelize para mayor limpieza
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  protocol: 'postgres'
 });
 
-// Conexión a la base de datos por medio de Sequelize
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Conexión a la base de datos exitosa.');
-    })
-    .catch(err => {
-        console.error('Error al conectar a la base de datos:', err);
-    });
+
+// Verificar la conexión
+const testConnection = async () => {
+  try {
+    console.log('Attempting to connect to the database...');
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+testConnection();
 
 export default sequelize;
-
-
