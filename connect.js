12a18c8e0d9ed3a166cbea466dd4422c4f461939
Edwin Estrada <r.1993andres.ee@gmail.com>
2024-08-21 22:59:59 -0500

    connect SupaBase, .env

--- connect.js
+++ connect.js
@@ -1,5 +1,27 @@
-import  Sequelize  from "sequelize"
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize ('postgres://postgres:12345@localhost:5432/modulo4')
+// Carga las variables de entorno desde el archivo .env
+dotenv.config();
 
-export default sequelize
+// Usa la URL de conexión proporcionada por Supabase desde las variables de entorno
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+
+// Conexión a la base de datos por medio de Sequelize
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Conectado a la base de datos con éxito.');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+  });
+
+export default sequelize;
