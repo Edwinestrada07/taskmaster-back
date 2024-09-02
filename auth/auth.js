3dd8f0b55644a1bc9bbd3ebe42b77b8618881254
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-09-02 10:11:33 -0500

    authMiddleware optimization

--- auth/auth.js
+++ auth/auth.js
@@ -5,75 +5,83 @@ import User from '../user/user.model.js';
 
 const app = express.Router();
 
-// Ruta para el registro de usuarios
+/**
+ * Ruta para el registro de usuarios.
+ * Valida los datos de entrada, verifica si el correo ya existe, 
+ * y crea un nuevo usuario con una contraseña encriptada.
+ */
 app.post('/signup', async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
+        // Verifica que los campos requeridos estén presentes
         if (!name || !email || !password) {
             return res.status(400).send('Faltan campos requeridos');
         }
 
-        const existingUser = await User.findOne({
-            where: {
-                email: email
-            }
-        });
-
+        // Verifica si el correo electrónico ya está registrado
+        const existingUser = await User.findOne({ where: { email } });
         if (existingUser) {
             return res.status(400).send('El correo electrónico ya existe');
         }
 
+        // Encripta la contraseña antes de guardarla
         const hashedPassword = await bcrypt.hash(password, 10);
 
+        // Crea el nuevo usuario en la base de datos
         const newUser = await User.create({
-            name: name,
-            email: email,
+            name,
+            email,
             password: hashedPassword,
         });
 
+        // Genera un token JWT para la autenticación
         const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
             expiresIn: '1h',
         });
 
+        // Respuesta exitosa con el token y los datos del usuario
         res.status(201).send({ token, user: { ...newUser.dataValues }, message: "Usuario registrado correctamente" });
 
     } catch (error) {
-        console.error('Error al registrar usuario', error);
+        console.error('Error al registrar usuario:', error);
         res.status(500).send('Error interno del servidor');
     }
 });
 
-
-// Ruta para iniciar sesión
+/**
+ * Ruta para iniciar sesión.
+ * Verifica las credenciales proporcionadas y genera un token JWT 
+ * si las credenciales son correctas.
+ */
 app.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({
-            where: {
-                email: req.body.email
-            }
-        });
+        const { email, password } = req.body;
+
+        // Busca al usuario por correo electrónico
+        const user = await User.findOne({ where: { email } });
         if (!user) {
             return res.status(400).send('Correo electrónico o contraseña incorrectos');
         }
 
-        // Comparar la contraseña proporcionada con el hash almacenado
-        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        // Compara la contraseña proporcionada con la almacenada
+        const isMatch = await bcrypt.compare(password, user.password);
         if (isMatch) {
+            // Genera un token JWT si la contraseña es correcta
             const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
                 expiresIn: '1h',
             });
-            
+
+            // Respuesta exitosa con el token y los datos del usuario
             res.status(200).send({ token, user: { ...user.dataValues }, message: 'Inicio de sesión correcto' });
         } else {
             res.status(400).send('Correo electrónico o contraseña incorrectos');
         }
 
     } catch (error) {
-        console.error('Error al iniciar sesión', error);
+        console.error('Error al iniciar sesión:', error);
         res.status(500).send('Error interno del servidor');
     }
 });
-
 
 export default app;
--- authMiddleware/authMiddleware.js
+++ authMiddleware/authMiddleware.js
@@ -1,19 +1,32 @@
 import jwt from 'jsonwebtoken';
 
+/**
+ * Middleware para validar el token JWT en las solicitudes.
+ * Si el token es válido, añade la información del usuario a `req.user` y llama a `next()`.
+ * Si el token es inválido o no está presente, se devuelve un error con el estado correspondiente.
+ */
 function validateToken(req, res, next) {
+    // Obtiene el token de la cabecera 'authorization'
     const token = req.headers['authorization'];
 
+    // Si no se proporciona un token, retorna un error 405 (Método no permitido)
     if (!token) {
         return res.status(405).send('No autorizado');
     }
 
     try {
+        // Verifica y decodifica el token usando la clave secreta almacenada en las variables de entorno
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Almacena la información del usuario en la solicitud para su uso posterior
         req.user = {
             id: decoded.id,
         };
+
+        // Llama a la siguiente función en la cadena de middleware
         next();
     } catch (error) {
+        // Manejo específico de errores según el tipo de error
         if (error.name === 'TokenExpiredError') {
             return res.status(405).send('Token expirado. Genere un nuevo token');
         } else {
@@ -23,3 +36,4 @@ function validateToken(req, res, next) {
 }
 
 export default validateToken;
+
